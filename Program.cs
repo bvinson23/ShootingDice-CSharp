f12d7448219282fcc38ddb19f5b2fa5b9cec9325
bvinson23 <brandonvinson23@gmail.com>
2021-06-07 14:33:47 -0500

    humanplayer complete

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace ShootingDice
     {
         static void Main(string[] args)
         {
-            Player player1 = new Player();
+            HumanPlayer player1 = new HumanPlayer();
             player1.Name = "Bob";
 
             OneHigherPlayer player2 = new OneHigherPlayer();
